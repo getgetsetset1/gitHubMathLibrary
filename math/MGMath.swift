ab498c58289d9635acc6d0a428b2c817d4fd12d0
getgetsetset1 <getgetsetset1@126.com>
2018-03-19 14:32:55 +0800

    add project

---
+++ math/MGMath.swift
@@ -0,0 +1,16 @@
+//
+//  MGMath.swift
+//  gitHubMathLibrary
+//
+//  Created by hello on 2018/3/19.
+//  Copyright © 2018年 hello. All rights reserved.
+//
+
+import Foundation
+
+class MGMath {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+}
